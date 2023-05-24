6f0f0a86bf5fbdb0444c23a39f8ad0e420357c45
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-05-23 22:59:44 -0600

    Polytropic EoS

--- SIMULATIONS/HD/WD/eos.c
+++ SIMULATIONS/HD/WD/eos.c
@@ -1,13 +1,10 @@
 #include"main.h"
 double eos(double valor, int i)
 {
-    //double Gamma=1.6666666666, k=3.119E12;
-    //double Gamma=1.33333333, k=4.883E14;
-    double Gamma=1.5, k=3.7E13;
     if(i==PRE){
-        return k*pow(valor,Gamma);
+        return polytropicK*pow(valor,polytropicExp);
     }
     if(i==RHO){
-        return pow(valor/k,1/Gamma);
+        return pow(valor/polytropicK,1/polytropicExp);
     }
 }
