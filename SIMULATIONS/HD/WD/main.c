a73f691893cb90175ed548bcb20e107ad515c662
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-05-23 23:52:30 -0600

    non modularized integrator

--- SIMULATIONS/HD/WD/main.c
+++ SIMULATIONS/HD/WD/main.c
@@ -1,28 +1,14 @@
 #include"main.h"
-#define PI 3.141592653589
-#define MB 2
 
 double  g(double x, double y[], int i);
-FILE *fp;
+
 int test_module(){
-    fp=fopen("gamma_15.dat", "a+");
     rk_order=2;
     rk_ rk;
     int j,k;
     rk.h=(x1max-x1min)/(Nx1-2*gc);
     double y[VX1+1], f[VX1+1], u1[VX1+1], u2[VX1+1];
-    printf("\n\n%.6e\n\n",rk.h);
-    float init_den=900000, final_den=2000000, step_den;
-    int mesh=10000;
-    step_den=(final_den-init_den)/mesh;
 
-    for (int a=0; a<=mesh; a++){
-    density = init_den+step_den*a;
-    for(int b = 0; b <= Nx1; b++){
-      U(RHO,b) = density;
-      U(PRE,b) = eos(density,PRE);
-      U(VX1,b) = 0;
-      }
     for (int i=gc; i<=Nx1-gc; i++){
         //Mi contador "extra" en 0 y no en ghost cells
         j=i-gc;
@@ -65,15 +51,11 @@ int test_module(){
         }
         u2[RHO]=eos(u2[PRE], RHO);
         if (u2[RHO]!=u2[RHO]){
-            fprintf(fp, "%.6e\t%.6e\t%.6e\n",density,grid.X1[i],U(MB,i));
-            printf("%.6e\t%.6e\t%.6e\t\t%.6e\t%.6e\n",density,grid.X1[i],U(MB,i),U(RHO,i), U(PRE,i));
             i=Nx1;
         }
         //Paso a U i+1
         for(k=0; k<=VX1; k++) U(k,i+1)=u2[k];
     }
-    }
-    fclose(fp);
 }
 double  g(double x, double y[], int i){
     //eos(x,y,RHO);
