cf7326b1cb00657489b94c5dedf60f482473acbc
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-05-23 23:02:23 -0600

    User global variables for mass density and EoS added

--- SIMULATIONS/HD/WD/user_param.h
+++ SIMULATIONS/HD/WD/user_param.h
@@ -23,9 +23,9 @@
 
 #define INTERFACE          DIAGONAL
 
-double rhol, pl, vx1l, vx2l, vx3l;
-double rhor, pr, vx1r, vx2r, vx3r;
-double x_0;
+#define PI 3.141592653589
+#define MB 2 //barionic mass
+
 int test_module();
 double eos(double valor, int i);
-float density;
+float massDensity, polytropicExp, polytropicK;
