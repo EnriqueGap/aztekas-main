86de27f6adf530f74563e91d5a57b6e76ae45857
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-07-23 18:49:17 -0600

    Debug finished for buildModel()

--- SIMULATIONS/RHD/WD/buildModel.c
+++ SIMULATIONS/RHD/WD/buildModel.c
@@ -5,7 +5,6 @@ int buildModel(){
     for (int i=gc; i<=Nx1-gc; i++){
 
         if (i==gc){
-            printf("\n\n\n%d\n\n\n",i);
             //When i==gc, radius=0
             //solve the ODE using Taylor Expansion for dP/dr
             ODE_Integration(i, 3, taylor_ode);
@@ -15,8 +14,6 @@ int buildModel(){
         }
 
         if(U(RHO,i+1)!=U(RHO,i+1)){
-            printf("\n\n\n%d\n\n\n",i);
-            system("read -p 'Press Enter to continue...' var");
             U(RHO,i+1)=0;
             U(PRE,i+1)=0;
             U(MB,i+1)=U(MB,i);
